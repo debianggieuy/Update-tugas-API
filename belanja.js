b4f6f445661a9d104d7a9422af4b77a4d8e56b9b
debianggieuy <debianggi549@gmail.com>
2022-01-27 08:46:12 +0700

    my project

---
+++ belanja.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+
+// set to read a request from JSON format
+
+app.use(express.json())
+// JSON is String
+
+app.post("/belanja", (request, response) => {
+    let barang = request.body.barang
+    let hasil = []
+
+    for (let i = 0; i < barang.length; i++) {
+        if (barang[i].qty >= barang[i].min_discount) {
+            let hargaAkhir = (barang[i].price * barang[i].qty) - (barang[i].price * barang[i].qty * barang[i].discount)
+            hasil.push({
+                "barang" : barang[i].name,
+                "total" : hargaAkhir,
+                "total.item" : barang[i].qty
+            })
+        } else if (barang[i].qty < barang[i].min_discount) {
+            let hargaAkhir = barang[i].price * barang[i].qty
+            hasil.push({
+                "barang" : barang[i].name,
+                "total" : hargaAkhir,
+                "total.item" : barang[i].qty
+            })
+        }
+    }
+
+    return response.json({
+        Belanja : hasil
+    })
+})
+
+app.listen(8000, () => {
+    console.log('Server run at port 8000');
+})
